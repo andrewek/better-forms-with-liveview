4dd9da2ee990d6febd1fa930993f5254358743a1
Andrew Ek <andrewek@gmail.com>
2023-05-18 00:58:09 -0500

    V4 of form

---
+++ lib/better_forms_web/live/invoice_live/new4.ex
@@ -0,0 +1,76 @@
+defmodule BetterFormsWeb.InvoiceLive.New4 do
+  @moduledoc """
+  Fourth example of form validation patterns
+
+  Relative to V3, we:
+
+  + Validate a little bit better
+  + Make use of the <.input> elements
+
+  Issues we still have:
+
+  + Still validate uniqueness of invoice number on every change
+  + Dirty/Clean state isn't quite right
+  + Still using amount_in_cents instead of something friendlier
+  """
+
+  use BetterFormsWeb, :live_view
+
+  alias BetterForms.Invoices.Context, as: InvoiceContext
+  alias BetterForms.Invoices.Invoice, as: Invoice
+
+  @impl true
+  def mount(_params, _session, socket) do
+    form =
+      %{}
+      |> Map.put(:invoice_number, InvoiceContext.recommended_invoice_number())
+      |> InvoiceContext.creation_changeset()
+      |> to_form()
+
+    status_options = InvoiceContext.status_options()
+
+    socket
+    |> assign(page_title: "New Invoice - 4")
+    |> assign(form: form)
+    |> assign(status_options: status_options)
+    |> then(&{:ok, &1})
+  end
+
+  @impl true
+  def handle_event("save", %{"invoice" => invoice_params}, socket) do
+    # :timer.sleep(2000)
+    changeset =
+      invoice_params
+      |> InvoiceContext.creation_changeset()
+
+    result = InvoiceContext.insert(changeset)
+
+    case result do
+      {:ok, %Invoice{invoice_number: number}} ->
+        socket
+        |> put_flash(:success, "Created invoice ##{number}!")
+        |> push_redirect(to: ~p"/invoices")
+        |> then(&{:noreply, &1})
+
+      {:error, changeset} ->
+        form = to_form(changeset)
+
+        socket
+        |> put_flash(:error, "Something went wrong!")
+        |> assign(form: form)
+        |> then(&{:noreply, &1})
+    end
+  end
+
+  def handle_event("validate", %{"invoice" => invoice_params}, socket) do
+    form =
+      invoice_params
+      |> InvoiceContext.creation_changeset()
+      |> Map.put(:action, :validate)
+      |> to_form()
+
+    socket
+    |> assign(form: form)
+    |> then(&{:noreply, &1})
+  end
+end
